a23345cd2d231e77a829b6195d1f2cc225b6e68d
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-11-22 18:23:57 +0530

    added cors

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import { graphqlHTTP as GraphQLHandler } from 'express-graphql';
 
 import Schema from './schema/Schema';
 
 const server = express();
 
+server.use(cors());
 server.use(morgan('dev'));
-server.use('/api', GraphQLHandler({ schema: Schema, graphiql: true }));
+
+server.use('/api', GraphQLHandler({ schema: Schema, graphiql: false }));
 
 server.listen(5000, () => {
   console.log('Server started');
